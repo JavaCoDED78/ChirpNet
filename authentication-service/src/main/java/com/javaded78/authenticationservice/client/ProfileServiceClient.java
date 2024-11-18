16cd35ecb65de64310395a242dafa6ae88ce2996
Andrei Soroka <131610832+JavaCoDED78@users.noreply.github.com>
2024-11-18 10:06:15 +0300

    [Feature] add ProfileController

--- authentication-service/src/main/java/com/javaded78/authenticationservice/client/ProfileServiceClient.java
+++ authentication-service/src/main/java/com/javaded78/authenticationservice/client/ProfileServiceClient.java
@@ -1,13 +1,12 @@
 package com.javaded78.authenticationservice.client;
 
-import com.javaded78.authenticationservice.dto.request.CreateProfileRequest;
-import com.javaded78.authenticationservice.exception.UserProfileNotFoundException;
+import com.javaded78.authenticationservice.dto.response.CreateProfileRequest;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "profile-service", url = "http://localhost:8088")
+@FeignClient(name = "profile-service", url = "http://localhost:8082")
 public interface ProfileServiceClient {
 
     @PostMapping(value = "/api/v1/profiles")
